34a9a8b2d6535ea34f2a32189a3906ff75977a70
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:39 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -5,19 +5,12 @@
     substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkPythonPackage = (import "${substrate}/lib/python-package.nix").mkPythonPackage;
-    in {
-      packages.default = mkPythonPackage pkgs {
-        pname = "network-diagram-generator";
-        version = "0.0.0-dev";
-        src = self;
-        description = "Akeyless network diagram generator for multi-tenant and single-tenant deployments";
-      };
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ python3 python3Packages.pip ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "python";
+    builder = "devShell";
+    description = "Akeyless network diagram generator for multi-tenant and single-tenant deployments";
+  };
 }
