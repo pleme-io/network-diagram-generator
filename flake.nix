20e76c995131ee878082dfa4f7fc9395dd1f4afc
drzln <luis.d@akeyless.io>
2026-06-08 18:03:55 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "Akeyless network diagram generator for multi-tenant and single-tenant deployments";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nixpkgs.follows = "substrate/nixpkgs";
+    substrate = { url = "github:pleme-io/substrate";};
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
